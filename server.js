662596d585fc00da42dbad0bfb0fe25274003db1
afzal <afzalmo722@gmail.com>
2026-01-14 12:17:55 +0530

    backend final polish

--- server.js
+++ server.js
@@ -39,7 +39,18 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.log("❌ MongoDB Error:", err.message));
 
-app.use(cors());
+app.use(cors({
+  origin: "https://cloud-vault-frontend-98mf.onrender.com",
+  methods: ["GET", "POST", "DELETE"],
+  credentials: true
+}));
+
+app.options("*", cors({
+  origin: "https://cloud-vault-frontend-98mf.onrender.com",
+  credentials: true
+}));
+
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "frontend")));
 
@@ -344,4 +355,6 @@ app.delete("/account/delete", auth, async (req, res) => {
 
 
 
-app.listen(5000, () => console.log("Backend running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Backend running on", PORT));
+
